62f31df61204da65ab02ba94e206f154ab06b343
dasha-alex <dashaaleksandrova20@gmail.com>
2025-09-26 21:44:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(){
   const char* secret = std::getenv("PASSWORD");
   std::string s(secret);
-  if (s = "qwerty") {
+  if (s == "qwerty") {
     std::cout << "Пароль верный!\n";
   }
   else {
