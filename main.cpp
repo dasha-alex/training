d2816d494af6971a74e88d40af3d8009e564b073
dasha-alex <dashaaleksandrova20@gmail.com>
2025-09-26 21:22:18 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main(){
+  const char* secret = std::getenv("PASSWORD");
+  std::string s(secret);
+  if (s = "qwerty") {
+    std::cout << "Пароль верный!\n";
+  }
+  else {
+    std::cout << "Пароль неверный!\n";
+  }
+  return 0;
+}
